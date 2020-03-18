52b7235ce91f593679ccc848ff54c16a5fd16b93
rieg-ec <rieg@uc.cl>
2020-03-18 02:14:44 -0300

    done with followers menu

--- Tareas/T00/Menus/followers.py
+++ Tareas/T00/Menus/followers.py
@@ -68,9 +68,41 @@ class FollowersMenu:
                     f.close()
 
 
-
-
-
-
     def unfollow(self):
-        user_to_unfollow = input("A quien desea dejar de seguir? : ")
+        user_to_unfollow = input("\nA quien desea dejar de seguir? : ")
+        with open('usuarios.csv', 'r') as f:
+            users = [i for i in f.read().split('\n')]
+            f.close()
+
+        if user_to_unfollow not in users:
+            print("\nUsuario no existente.")
+
+        else:
+            with open('seguidores.csv', 'r+') as f:
+                user_followers = [i.split(',', 1) for i in f.read().split('\n')] #[[user, followers]] if following someone
+                followers_list = [i[1].split(',') for i in user_followers if i[0] == self.logged_user and len(i) > 1 and i[1] != ""]
+                f.close()
+
+            if len(followers_list) == 0 or user_to_unfollow not in followers_list[0]:
+                print("\nNo sigue a este usuario")
+
+            else:
+
+                followers_list[0].remove(user_to_unfollow) # it may result in empty list
+
+                with open('seguidores.csv', 'w') as f:
+                    for i in user_followers:
+                        if i[0] != self.logged_user:
+                            if len(i) > 1 and i[1] != "":
+                                str = "{},{}\n".format(i[0], i[1])
+                                f.write(str)
+                            else:
+                                f.write(i[0] + '\n')
+                        else:
+                            if len(followers_list[0]) > 0:
+                                str = "{},{}".format(self.logged_user, ",".join(i for i in followers_list[0]))
+                                f.write(str)
+                            else:
+                                f.write(self.logged_user + '\n')
+
+                    f.close()
