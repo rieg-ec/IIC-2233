cb6aefe848d3b497d39123a9025bb90d4f09caf4
rieg-ec <rieg@uc.cl>
2020-04-23 14:44:32 -0400

    funcion obtener_clientes, categorizar y calcular_precio listas

---
+++ Actividades/AS02/funcionalidades.py
@@ -0,0 +1,44 @@
+from functools import reduce
+# En este archivo tendrás que completar las funciones entregadas.
+
+
+def desencriptar(cliente_encriptado):
+    # No modificar
+    nombre_encriptado = list(cliente_encriptado[1])
+    letras = list('consumir')
+    numeros = [str(i) for i in range(1, 9)]
+    clave = [list(i) for i in zip(letras, numeros)]
+    for i in range(len(nombre_encriptado)):
+        for elemento in clave:
+            if nombre_encriptado[i] in elemento:
+                if nombre_encriptado[i] == elemento[0]:
+                    nombre_encriptado[i] = elemento[1]
+                elif nombre_encriptado[i] == elemento[1]:
+                    nombre_encriptado[i] = elemento[0]
+    nombre_desencriptado = ''.join(nombre_encriptado)
+    cliente_desencriptado = cliente_encriptado
+    cliente_desencriptado[1] = nombre_desencriptado
+    return cliente_desencriptado
+
+def obtener_clientes(lista_clientes_encriptados):
+    lista_desencriptada = map(lambda x: desencriptar(x),
+                              lista_clientes_encriptados)
+    return list(lista_desencriptada)
+
+
+def categorizar(productos, categoria):
+    prod_cat = filter(lambda x: x == categoria,
+                      productos)
+
+    return prod_cat
+
+
+def calcular_precio(productos):
+    precio_total = reduce(lambda x, y: x + y.precio,
+                          productos, 0)
+    return precio_total
+
+
+def generar_productos_disponibles(clientes):
+    # Completar
+    pass
